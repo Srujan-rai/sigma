c188d38ec35bc0748fd220f6a7d0377861015e5a
Aman-Shetty <amanshetty370@gmail.com>
2024-11-23 21:18:13 +0530

    Changed the font of navbar

--- sigma/src/components/Navbar.jsx
+++ sigma/src/components/Navbar.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive((prev) => !prev); // Toggle the state of the menu
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <a href="#home" className="navbar-logo">SIGMA</a>
-        <ul className="navbar-links">
+        <div className="hamburger" onClick={toggleMenu}>
+          {/* Hamburger Icon */}
+          <div className={`line ${menuActive ? "open" : ""}`}></div>
+          <div className={`line ${menuActive ? "open" : ""}`}></div>
+          <div className={`line ${menuActive ? "open" : ""}`}></div>
+        </div>
+        <ul className={`navbar-links ${menuActive ? "active" : ""}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#achievements">Achievements</a></li>
           <li><a href="#faculty">Faculty</a></li>
