c2a42ead7d803805710e9f18c82e91bed3fbc29f
Srujan-rai <raisrujan@gmail.com>
2024-11-23 12:20:49 +0530

    background added

---
+++ sigma/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BackgroundAnimation from './components/BackgroundAnimation';
+
+function App() {
+  return (
+    <>
+      <BackgroundAnimation />
+      <h1 style={{ color: 'white', textAlign: 'center', marginTop: '50px' }}>
+        Sigma
+      </h1>
+    </>
+  );
+}
+
+export default App;
